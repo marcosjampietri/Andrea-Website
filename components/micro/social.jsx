3e5199c089101ba2c18a967da6c91aaec82e6d4e
MARCOS JAMPIETRI <marcosjampietri@marcoss-macbook-pro.local>
2021-07-22 15:32:03 +0000

    test

--- components/micro/social.jsx
+++ components/micro/social.jsx
@@ -4,137 +4,129 @@ import { RevealR } from "../reveal";
 import { below } from "../../styles/breakpoint";
 
 const Social = () => {
-    const social = [
-        {
-            name: "@andrea_mangiacavallo",
-            icon: "facebook.svg",
-            url: "https://github.com/marcosjampietri/",
-        },
-        {
-            name: "@andrea_mangiacavallo",
-            icon: "youtube.svg",
-            url: "https://github.com/marcosjampietri/",
-        },
-        {
-            name: "@andrea_mangiacavallo",
-            icon: "instagram.svg",
-            url: "https://github.com/marcosjampietri/",
-        },
-        {
-            name: "@andrea_mangiacavallo",
-            icon: "twitter.svg",
-            url: "https://github.com/marcosjampietri/",
-        },
-    ];
+  const social = [
+    {
+      name: "FACEBOOK",
+      icon: "facebook.svg",
+      url: "https://www.facebook.com/andre.siffredi",
+    },
+    {
+      name: "YOUTUBE",
+      icon: "youtube.svg",
+      url: "https://www.youtube.com/channel/UCFjLZ79E8WI8FyrUInjdAbg",
+    },
+    {
+      name: "INSTAGRAM",
+      icon: "instagram.svg",
+      url: "https://www.instagram.com/andrea_mangiacavallo/",
+    },
+    {
+      name: "LINKEDIN",
+      icon: "linkedin.svg",
+      url: "https://www.linkedin.com/in/andrea-mangiacavallo-ab8009142/",
+    },
+  ];
 
-    return (
-        <Block>
-            <RevealR>
-                <Title>
-                    <h3>SOCIAL MEDIA</h3>
-                </Title>
-                {social.map((item, index) => (
-                    <Tab
-                        href={item.url}
-                        target="_blank"
-                        rel="noreferrer"
-                        key={index}>
-                        <h6>{item.name}</h6>
-                        <img
-                            src={`/icons/${item.icon}`}
-                            alt="icon"
-                            height="30px"
-                            width="30px"
-                        />
-                    </Tab>
-                ))}
-            </RevealR>
-        </Block>
-    );
+  return (
+    <Block>
+      <RevealR>
+        <Title>
+          <h3>SOCIAL MEDIA</h3>
+        </Title>
+        {social.map((item, index) => (
+          <Tab href={item.url} target="_blank" rel="noreferrer" key={index}>
+            <h6>{item.name}</h6>
+            <img
+              src={`/icons/${item.icon}`}
+              alt="icon"
+              height="30px"
+              width="30px"
+            />
+          </Tab>
+        ))}
+      </RevealR>
+    </Block>
+  );
 };
 
 export default Social;
 
 const Block = styled.div`
-    display: block;
-    position: fixed;
-    width: 250px;
-    top: 20vh;
-    right: -100px;
+  display: block;
+  position: fixed;
+  width: 250px;
+  top: 20vh;
+  right: -100px;
 
-    ${below.med`
+  ${below.med`
         display: none;
 
     `}
 `;
 const Title = styled.div`
-    position: absolute;
-    top: 150px;
-    left: 0px;
-    transform: rotate(90deg);
-    width: 200px;
-    text-align: center;
-    padding-bottom: 10px;
+  position: absolute;
+  top: 150px;
+  left: 0px;
+  transform: rotate(90deg);
+  width: 200px;
+  text-align: center;
+  padding-bottom: 10px;
 
-    border-bottom: 1px solid grey;
+  border-bottom: 1px solid grey;
 `;
 
 const Tab = styled.a`
-    width: 50px;
-    height: 50px;
-    margin: 40px 0px;
+  width: 50px;
+  height: 50px;
+  margin: 40px 0px;
 
-    transform: translate(-100%, 0px);
-    transform-origin: top right;
-    opacity: 0.4;
-    overflow: hidden;
+  transform: translate(-100%, 0px);
+  transform-origin: top right;
+  opacity: 0.4;
+  overflow: hidden;
 
-    background-image: linear-gradient(
-        to right,
-        hsla(220, 10%, 100%, 0.02),
-        hsla(220, 10%, 100%, 0.85)
-    );
+  background-image: linear-gradient(
+    to right,
+    hsla(220, 10%, 100%, 0.02),
+    hsla(220, 10%, 100%, 0.85)
+  );
+  transition: 1s;
+  transition-timing-function: cubic-bezier(0.98, 0.42, 0, 0.99);
+
+  border-radius: 5px;
+
+  position: relative;
+  top: 0;
+  right: -50px;
+  display: flex;
+  align-items: center;
+
+  img {
+    position: absolute;
+    top: 10px;
+    right: 5px;
+    width: 40px;
+
+    transform: rotate(90deg);
+    filter: grayscale(100%);
     transition: 1s;
-    transition-timing-function: cubic-bezier(0.98, 0.42, 0, 0.99);
+  }
+  h6 {
+    position: absolute;
+    right: 60px;
+  }
+  &:hover {
+    transition: 1s;
+    transition-timing-function: cubic-bezier(0.98, 0, 0, 0.99);
 
-    border-radius: 5px;
-
-    position: relative;
-    top: 0;
-    right: -50px;
-    display: flex;
-    align-items: center;
-
-
-
+    width: 100%;
+    opacity: 1;
     img {
-        position: absolute;
-        top: 10px;
-        right: 5px;
-        width: 40px;
-
-        transform: rotate(90deg);
-        filter: grayscale(100%);
-        transition: 1s;
-
+      transition: 0.2s;
+      transform: rotate(0deg);
+      filter: grayscale(0%);
     }
-    h6 {
-        position: absolute;
-        right: 50px;
-    }
-    &:hover {
-        transition: 1s;
-        transition-timing-function: cubic-bezier(0.98, 0, 0, 0.99);
+  }
 
-        width: 100%;
-        opacity: 1;
-        img {
-            transition: 0.2s;
-            transform: rotate(0deg);
-            filter: grayscale(0%);
-
-        }
-    }
-
-    z-index: 10;
+  z-index: 10;
 `;
--- components/section/Footer.jsx
+++ components/section/Footer.jsx
@@ -1,105 +1,105 @@
 import styled from "styled-components";
 
 const Footer = () => {
-    return (
-        <FooterSt>
-            <Container>
-                <div>
-                    <Column>
-                        <aside>
-                            <h3>ADDRESS</h3>
-                            <p>
-                                Welcome to my creative lab. Lorem ipsum is
-                                simply free text dolor sit amet consectetur
-                                adipisicing elit. Tempore corrupti temporibus
-                                fuga earum asperiores, alias excepturi sit
-                                mpedit fugit laudantium.
-                            </p>
-                        </aside>
-                    </Column>
-                    <Column>
-                        <aside>
-                            <h3>CONTACT</h3>
-                            <p>
-                                41 Broklyn Street, New York United States of
-                                America
-                            </p>
-                            <Pair>
-                                <p>Phone:</p>
-                                <p>666 888 000</p>
-                            </Pair>
-                            <Pair>
-                                <p>Email:</p>
-                                <a href="#"> andreamangiacavallo@gmail.com </a>
-                            </Pair>
-                        </aside>
-                    </Column>
-                    <Column>
-                        <Social>
-                            <h3>SOCIAL</h3>
-                            <ul>
-                                <li>
-                                    <a href="#">
-                                        <img
-                                            src="/icons/facebook.svg"
-                                            alt="Twitter Logo"
-                                        />
-                                        <p>Facebook</p>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <img
-                                            src="/icons/instagram.svg"
-                                            alt="Twitter Logo"
-                                        />
-                                        <p>Instagram</p>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <img
-                                            src="/icons/youtube.svg"
-                                            alt="Twitter Logo"
-                                        />
-                                        <p>Youtube</p>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <img
-                                            src="/icons/twitter.svg"
-                                            alt="Twitter Logo"
-                                        />
-                                        <p>Twitter</p>
-                                    </a>
-                                </li>
-                            </ul>
-                        </Social>
-                    </Column>
-                </div>
-            </Container>
-            <CopyR>
-                <div>
-                    <p> © copyright 2021 by </p>
-                    <a href="#">marcosdepaiva.com</a>
-                </div>
-            </CopyR>
-        </FooterSt>
-    );
+  return (
+    <FooterSt>
+      <Container>
+        <div>
+          <Column>
+            <aside>
+              <h3>ADDRESS</h3>
+              <p>
+                Welcome to my creative lab. Lorem ipsum is simply free text
+                dolor sit amet consectetur adipisicing elit. Tempore corrupti
+                temporibus fuga earum asperiores, alias excepturi sit mpedit
+                fugit laudantium.
+              </p>
+            </aside>
+          </Column>
+          <Column>
+            <aside>
+              <h3>CONTACT</h3>
+              <p>Camden Town, London, UK</p>
+              <Pair>
+                <p>Phone:</p>
+                <p>+44 7440450444</p>
+              </Pair>
+              <Pair>
+                <p>Email:</p>
+                <h5> andrea.mangiacavallo@gmail.com </h5>
+              </Pair>
+            </aside>
+          </Column>
+          <Column>
+            <Social>
+              <h3>SOCIAL</h3>
+              <ul>
+                <li>
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.facebook.com/andre.siffredi"
+                  >
+                    <img src="/icons/facebook.svg" alt="Twitter Logo" />
+                    <p>Facebook</p>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.instagram.com/andrea_mangiacavallo/"
+                  >
+                    <img src="/icons/instagram.svg" alt="Twitter Logo" />
+                    <p>Instagram</p>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.youtube.com/channel/UCFjLZ79E8WI8FyrUInjdAbg"
+                  >
+                    <img src="/icons/youtube.svg" alt="Twitter Logo" />
+                    <p>Youtube</p>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.linkedin.com/in/andrea-mangiacavallo-ab8009142/"
+                  >
+                    <img src="/icons/linkedin.svg" alt="LinkedIn Logo" />
+                    <p>Twitter</p>
+                  </a>
+                </li>
+              </ul>
+            </Social>
+          </Column>
+        </div>
+      </Container>
+      <CopyR>
+        <div>
+          <p> © copyright 2021 by </p>
+          <a href="#">marcosdepaiva.com</a>
+        </div>
+      </CopyR>
+    </FooterSt>
+  );
 };
 
 export default Footer;
 
 const FooterSt = styled.section`
-    width: 100vw;
-    padding: 100px 0px;
+  width: 100vw;
+  padding: 100px 0px;
 
-    background-color: hsla(0, 0%, 10%, 1);
+  background-color: hsla(0, 0%, 10%, 1);
 
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
 `;
 
 const Container = styled.div`
@@ -129,47 +129,47 @@ margin-bottom: 50px;
 `;
 
 const Social = styled.aside`
-    ul {
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+
+    li {
+      padding: 0px 40px 0px 0px;
+      width: 170px;
+      a {
         display: flex;
-        flex-wrap: wrap;
+        filter: grayscale(100%);
+        transition: 0.8s;
+        p {
+          transition: 0.8s;
 
-        li {
-            padding: 0px 40px 0px 0px;
-            width: 170px;
-            a {
-                display: flex;
-                filter: grayscale(100%);
-                transition: 0.8s;
-                p {
-                    transition: 0.8s;
-
-                    color: hsla(345, 0%, 50%, 1);
-                }
-                :hover {
-                    transition: 0.2s;
-                    filter: grayscale(0%);
-                    p {
-                        transition: 0.2s;
-                        color: hsla(345, 100%, 60%, 1);
-                    }
-                    img {
-                        transition: 0.2s;
-                        background-color: hsla(0, 0%, 100%, 0.8);
-                    }
-                }
-                img {
-                    background-color: hsla(0, 0%, 100%, 0.2);
-
-                    margin: 10px 25px 10px 0px;
-                    padding: 2px;
-                    width: 35px;
-                    height: 35px;
-
-                    border-radius: 5px;
-                }
-            }
+          color: hsla(345, 0%, 50%, 1);
         }
+        :hover {
+          transition: 0.2s;
+          filter: grayscale(0%);
+          p {
+            transition: 0.2s;
+            color: hsla(345, 100%, 60%, 1);
+          }
+          img {
+            transition: 0.2s;
+            background-color: hsla(0, 0%, 100%, 0.8);
+          }
+        }
+        img {
+          background-color: hsla(0, 0%, 100%, 0.2);
+
+          margin: 10px 25px 10px 0px;
+          padding: 2px;
+          width: 35px;
+          height: 35px;
+
+          border-radius: 5px;
+        }
+      }
     }
+  }
 `;
 
 const Column = styled.div`
@@ -211,30 +211,30 @@ const Column = styled.div`
 `;
 
 const Pair = styled.div`
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 
-    p {
-        padding: 0px 20px 0px 0px;
-    }
+  p {
+    padding: 0px 20px 0px 0px;
+  }
 `;
 
 const CopyR = styled.div`
-    width: 100%;
-    height: 10vh;
+  width: 100%;
+  height: 10vh;
 
-    div {
-        width: 80%;
-        margin: 0 auto;
-        padding: 20px;
-        text-align: center;
+  div {
+    width: 80%;
+    margin: 0 auto;
+    padding: 20px;
+    text-align: center;
 
-        border-top: 1px solid hsla(0, 0%, 20%, 1);
-        P,
-        a {
-            color: #bebebe;
-            margin-bottom: 10px;
-            letter-spacing: 3px;
-        }
+    border-top: 1px solid hsla(0, 0%, 20%, 1);
+    P,
+    a {
+      color: #bebebe;
+      margin-bottom: 10px;
+      letter-spacing: 3px;
     }
+  }
 `;
